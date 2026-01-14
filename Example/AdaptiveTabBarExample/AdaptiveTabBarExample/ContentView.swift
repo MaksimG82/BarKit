9833219f8172ad734cc19dc18b7954e563db6c25
Maksim Gaisin <maksim@gaisin.space>
2026-01-14 11:22:36 +0100

    initial example xcode project added

---
+++ Example/AdaptiveTabBarExample/AdaptiveTabBarExample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AdaptiveTabBarExample
+//
+//  Created by Maksim Gaisin on 12.01.26.
+//
+
+import SwiftUI
+import AdaptiveTabBar
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
