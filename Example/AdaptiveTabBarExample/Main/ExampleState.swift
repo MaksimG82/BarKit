2cc4475b646640147ccbb6b5fcc277ef867b2ddf
Maksim Gaisin <maksim@gaisin.space>
2026-03-13 11:15:07 +0100

    [ATB-17] ExampleState style

--- Example/AdaptiveTabBarExample/Main/ExampleState.swift
+++ Example/AdaptiveTabBarExample/Main/ExampleState.swift
@@ -9,20 +9,30 @@ import AdaptiveTabBar
 import SwiftUI
 
 /// Represents the entire state of the Example app screen.
+/// Designed to be used within a ViewModel to drive the UI in a unidirectional flow.
 struct ExampleState {
-    /// The current configuration of the TabBar.
-    var config: TabBarConfiguration = .init()
+    // MARK: - Configuration
 
-    /// The list of tab items to display.
-    var items: [ExampleTabItem] = ExampleTabItem.fiveItems
+    /// The visual configuration of the TabBar.
+    var config: TabBarConfiguration = .init()
 
     /// Flag for visual layout debugging.
     var isDebugLayoutEnabled: Bool = false
 
-    /// The currently selected tab.
+    // MARK: - Data & Selection
+
+    /// The list of tab items currently displayed in the bar.
+    var items: [ExampleTabItem] = ExampleTabItem.fiveItems
+
+    /// The currently active tab.
     var selectedTab: ExampleTabItem
 
+    // MARK: - Initialization
+
+    /// Initializes the state with default items and selects the first one.
     init() {
-        selectedTab = items.first ?? .init(type: .home, style: .regular)
+        let defaultItems = ExampleTabItem.fiveItems
+        items = defaultItems
+        selectedTab = defaultItems.first ?? .init(type: .home, style: .regular)
     }
 }
